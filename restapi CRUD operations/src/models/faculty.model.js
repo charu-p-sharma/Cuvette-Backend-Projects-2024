597d7dcdb7c1c3c9036ca8a29988e8b04d3272d3
charu <charupsharma5@gmail.com>
2024-06-26 11:12:19 +0530

    faculty model created

--- restapi CRUD operations/src/models/faculty.model.js
+++ restapi CRUD operations/src/models/faculty.model.js
@@ -1,3 +1,22 @@
 import mongoose from "mongoose";
 
-const Faculty = new Schema.Faculty
+const facultySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    department: {
+        type: String,
+        required: true
+    },
+    designation: {
+        type: String,
+        required: true
+    },
+}, { timestamps: true })
+
+export const Faculty = mongoose.model('Faculty', facultySchema);
